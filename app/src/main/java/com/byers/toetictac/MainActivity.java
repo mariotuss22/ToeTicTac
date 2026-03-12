da529108206cb29283846f9018f472ef6e6a4466
wyatt byers <paleboi528@gmail.com>
2026-03-12 14:13:15 -0400

    screens and buttons made

--- app/src/main/java/com/byers/toetictac/MainActivity.java
+++ app/src/main/java/com/byers/toetictac/MainActivity.java
@@ -1,6 +1,9 @@
 package com.byers.toetictac;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView titleTextView = findViewById(R.id.titleTextView);
+        Button startButton = findViewById(R.id.start_Button);
+        Button settingsButton = findViewById(R.id.settings_Button);
+
+
+
+        startButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Menu.class);
+            startActivity(intent);
+        });
+
     }
+
+
+
+
+
 }
