195be24381647a26a56a31ac528d651b5c78c883
wyatt byers <paleboi528@gmail.com>
2026-03-18 13:52:07 -0400

    improved buttons and such

--- app/src/main/java/com/byers/toetictac/Menu.java
+++ app/src/main/java/com/byers/toetictac/Menu.java
@@ -1,6 +1,11 @@
 package com.byers.toetictac;
 
+import static java.util.Calendar.getInstance;
+
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,12 +16,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import org.w3c.dom.Text;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Menu extends AppCompatActivity {
 
     boolean player1Turn = false;
-    boolean player2Turm = true;
+    boolean player2Turn = true;
+    TextView turnAlert;
+    public ArrayList<Button> XOButtons = new ArrayList<>();
+    Resources res;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,191 +51,232 @@ public class Menu extends AppCompatActivity {
         Button button7 = findViewById(R.id.button7);
         Button button8 = findViewById(R.id.button8);
         TextView turnAlert = findViewById(R.id.turnAlert);
-        button0.setOnClickListener(new View.OnClickListener() {
-            @Override
-                    public void onClick(View v) {
-                    if (player1Turn) {
-                        button0.setText("X");
-                        button0.setClickable(false);
-                        player2Turm = true;
-                        player1Turn = false;
-                        changeTurnAlert(turnAlert);
-                } else if (player2Turm) {
-                        button0.setText("o");
-                        button0.setClickable(false);
-                        player1Turn = true;
-                        player2Turm = false;
-                        changeTurnAlert(turnAlert);
-                    }
-            }
 
-        });
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (player1Turn) {
-                    button1.setText("X");
-                    button1.setClickable(false);
-                    player2Turm = true;
-                    player1Turn = false;
-                    changeTurnAlert(turnAlert);
-                } else if (player2Turm) {
-                    button1.setText("o");
-                    button1.setClickable(false);
-                    player1Turn = true;
-                    player2Turm = false;
-                    changeTurnAlert(turnAlert);
-                }
-            }
+        initializeButtons();
 
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (player1Turn) {
-                    button2.setText("X");
-                    button2.setClickable(false);
-                    player2Turm = true;
-                    player1Turn = false;
-                    changeTurnAlert(turnAlert);
-                } else if (player2Turm) {
-                    button2.setText("o");
-                    button2.setClickable(false);
-                    player1Turn = true;
-                    player2Turm = false;
-                    changeTurnAlert(turnAlert);
-                }
-            }
 
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (player1Turn) {
-                    button3.setText("X");
-                    button3.setClickable(false);
-                    player2Turm = true;
-                    player1Turn = false;
-                    changeTurnAlert(turnAlert);
-
-                } else if (player2Turm) {
-                    button3.setText("o");
-                    button3.setClickable(false);
-                    player1Turn = true;
-                    player2Turm = false;
-                    changeTurnAlert(turnAlert);
-                }
-            }
-
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (player1Turn) {
-                    button4.setText("X");
-                    button4.setClickable(false);
-                    player2Turm = true;
-                    player1Turn = false;
-                    changeTurnAlert(turnAlert);
-                } else if (player2Turm) {
-                    button4.setText("o");
-                    button4.setClickable(false);
-                    player1Turn = true;
-                    player2Turm = false;
-                    changeTurnAlert(turnAlert);
-                }
-            }
-
-        });
-
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (player1Turn) {
-                    button5.setText("X");
-                    button5.setClickable(false);
-                    player2Turm = true;
-                    player1Turn = false;
-                    changeTurnAlert(turnAlert);
-                } else if (player2Turm) {
-                    button5.setText("o");
-                    button5.setClickable(false);
-                    player1Turn = true;
-                    player2Turm = false;
-                    changeTurnAlert(turnAlert);
-                }
-            }
-
-        });
-
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (player1Turn) {
-                    button6.setText("X");
-                    button6.setClickable(false);
-                    player2Turm = true;
-                    player1Turn = false;
-                    changeTurnAlert(turnAlert);
-                } else if (player2Turm) {
-                    button6.setText("o");
-                    button6.setClickable(false);
-                    player1Turn = true;
-                    player2Turm = false;
-                    changeTurnAlert(turnAlert);
-                }
-            }
-
-        });
-
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (player1Turn) {
-                    button7.setText("X");
-                    button7.setClickable(false);
-                    player2Turm = true;
-                    player1Turn = false;
-                    changeTurnAlert(turnAlert);
-                } else if (player2Turm) {
-                    button7.setText("o");
-                    button7.setClickable(false);
-                    player1Turn = true;
-                    player2Turm = false;
-                    changeTurnAlert(turnAlert);
-                }
-            }
-
-        });
-
-        button8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (player1Turn) {
-                    button8.setText("X");
-                    button8.setClickable(false);
-                    player2Turm = true;
-                    player1Turn = false;
-                    changeTurnAlert(turnAlert);
-
-                } else if (player2Turm) {
-                    button8.setText("o");
-                    button8.setClickable(false);
-                    player1Turn = true;
-                    player2Turm = false;
-                    changeTurnAlert(turnAlert);
-                }
-            }
-
-        });
+//        button0.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//                    public void onClick(View v) {
+//                    if (player1Turn) {
+//                        button0.setText("X");
+//                        button0.setClickable(false);
+//                        player2Turn = true;
+//                        player1Turn = false;
+//                        changeTurnAlert(turnAlert);
+//                } else if (player2Turn) {
+//                        button0.setText("o");
+//                        button0.setClickable(false);
+//                        player1Turn = true;
+//                        player2Turn = false;
+//                        changeTurnAlert(turnAlert);
+//                    }
+//            }
+//
+//        });
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (player1Turn) {
+//                    button1.setText("X");
+//                    button1.setClickable(false);
+//                    player2Turn = true;
+//                    player1Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                } else if (player2Turn) {
+//                    button1.setText("o");
+//                    button1.setClickable(false);
+//                    player1Turn = true;
+//                    player2Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                }
+//            }
+//
+//        });
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (player1Turn) {
+//                    button2.setText("X");
+//                    button2.setClickable(false);
+//                    player2Turn = true;
+//                    player1Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                } else if (player2Turn) {
+//                    button2.setText("o");
+//                    button2.setClickable(false);
+//                    player1Turn = true;
+//                    player2Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                }
+//            }
+//
+//        });
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (player1Turn) {
+//                    button3.setText("X");
+//                    button3.setClickable(false);
+//                    player2Turn = true;
+//                    player1Turn = false;
+//                    changeTurnAlert(turnAlert);
+//
+//                } else if (player2Turn) {
+//                    button3.setText("o");
+//                    button3.setClickable(false);
+//                    player1Turn = true;
+//                    player2Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                }
+//            }
+//
+//        });
+//
+//        button4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (player1Turn) {
+//                    button4.setText("X");
+//                    button4.setClickable(false);
+//                    player2Turn = true;
+//                    player1Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                } else if (player2Turn) {
+//                    button4.setText("o");
+//                    button4.setClickable(false);
+//                    player1Turn = true;
+//                    player2Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                }
+//            }
+//
+//        });
+//
+//        button5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (player1Turn) {
+//                    button5.setText("X");
+//                    button5.setClickable(false);
+//                    player2Turn = true;
+//                    player1Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                } else if (player2Turn) {
+//                    button5.setText("o");
+//                    button5.setClickable(false);
+//                    player1Turn = true;
+//                    player2Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                }
+//            }
+//
+//        });
+//
+//        button6.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (player1Turn) {
+//                    button6.setText("X");
+//                    button6.setClickable(false);
+//                    player2Turn = true;
+//                    player1Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                } else if (player2Turn) {
+//                    button6.setText("o");
+//                    button6.setClickable(false);
+//                    player1Turn = true;
+//                    player2Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                }
+//            }
+//
+//        });
+//
+//        button7.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (player1Turn) {
+//                    button7.setText("X");
+//                    button7.setClickable(false);
+//                    player2Turn = true;
+//                    player1Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                } else if (player2Turn) {
+//                    button7.setText("o");
+//                    button7.setClickable(false);
+//                    player1Turn = true;
+//                    player2Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                }
+//            }
+//
+//        });
+//
+//        button8.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (player1Turn) {
+//                    button8.setText("X");
+//                    button8.setClickable(false);
+//                    player2Turn = true;
+//                    player1Turn = false;
+//                    changeTurnAlert(turnAlert);
+//
+//                } else if (player2Turn) {
+//                    button8.setText("o");
+//                    button8.setClickable(false);
+//                    player1Turn = true;
+//                    player2Turn = false;
+//                    changeTurnAlert(turnAlert);
+//                }
+//            }
+//
+//        });
 
     }
+
+    public void initializeButtons() {
+        String buttonID;
+        res = getResources();
+        res = getResources();
+
+
+
+        for (int i = 0; i < 8; i++) {
+            buttonID = "button" + i;
+            int resID = res.getIdentifier(buttonID, "id", getPackageName());
+            XOButtons.add(findViewById(resID));
+            XOButtons.get(i).setVisibility(View.VISIBLE);
+            XOButtons.get(i).setPadding(5, 5, 5, 5);
+            XOButtons.get(i).setText(String.valueOf(i + 1));
+
+
+            XOButtons.get(i).setOnClickListener(v -> {
+                changeButton();
+            });
+        }
+    }
+
+    public void changeButton() {
+        initializeButtons();
+        for (int i = 0; i <8; i++) {
+            if (player1Turn) {
+                XOButtons.get(i).setText("X");
+                XOButtons.get(i).setClickable(false);
+                player2Turn = true;
+                player1Turn = false;
+                changeTurnAlert(turnAlert);
+            }
+        }
+    }
+
+
 
     public void changeTurnAlert(TextView alert) {
         if (player1Turn) {
             alert.setText("Player 1 turn");
-        } else if (player2Turm) {
+        } else if (player2Turn) {
             alert.setText("Player 2 turn");
         }
     }
